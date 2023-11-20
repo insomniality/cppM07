d56543c69dfbe70b7998b555e43d33d464c9e6b3
Michael Arzumanyan <michaaelarzumanyan@gmail.com>
2023-11-20 20:52:05 +0400

    barev2

--- ex01/iter.hpp
+++ ex01/iter.hpp
@@ -3,14 +3,6 @@
 #include<iostream>
 
 template<typename T>
-
-void func(T a)
-{
-	std::cout << a;
-	std::cout << std::endl;
-}
-
-template<typename T>
 void iter(T* a, size_t l, void (*func)(const T&))
 {
 	for (size_t i = 0; i < l; i++)
--- ex01/main.cpp
+++ ex01/main.cpp
@@ -1,5 +1,13 @@
 #include "iter.hpp"
 
+template<typename T>
+
+void func(T a)
+{
+	std::cout << a;
+	std::cout << std::endl;
+}
+
 int main()
 {
 	int a[] = {1, 3, 2};
